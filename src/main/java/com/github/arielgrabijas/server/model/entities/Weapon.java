d6904e2eb9154469f7a5ba0e384146f7cf1950c1
agrabijas <agrabijas@advatech.pl>
2019-09-09 17:48:27 +0200

    unidirectional oneToMany relation done

--- src/main/java/com/github/arielgrabijas/server/model/entities/Weapon.java
+++ src/main/java/com/github/arielgrabijas/server/model/entities/Weapon.java
@@ -1,26 +1,23 @@
 package com.github.arielgrabijas.server.model.entities;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
 @Entity
-public class Weapon {
+public class Weapon implements Serializable {
 
     private static final long serialVersionUID = 2L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_weapon")
     private Integer id;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "id", nullable = false)
-    private Fellowshipmember fellowshipmember;
 
     @NotNull
     private String name;
@@ -39,13 +36,5 @@ public class Weapon {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public Fellowshipmember getFellowshipmember() {
-        return fellowshipmember;
-    }
-
-    public void setFellowshipmember(Fellowshipmember fellowshipmember) {
-        this.fellowshipmember = fellowshipmember;
     }
 }
